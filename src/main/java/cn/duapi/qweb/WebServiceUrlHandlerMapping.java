269c34eeb6492da4ed83b79a5da629cdae8b183a
qinwei <imqinwei@qq.com>
2019-12-27 18:20:32 +0800

    精简

--- src/main/java/cn/duapi/qweb/WebServiceUrlHandlerMapping.java
+++ src/main/java/cn/duapi/qweb/WebServiceUrlHandlerMapping.java
@@ -6,33 +6,32 @@ import org.springframework.beans.BeansException;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
 /**
- * 
  * @author qinwei
- * 
  */
 public class WebServiceUrlHandlerMapping extends SimpleUrlHandlerMapping {
-	@Override
-	protected void registerHandlers(Map<String, Object> urlMap) throws BeansException {
-		if (urlMap.isEmpty()) {
-			logger.warn("Neither 'urlMap' nor 'mappings' set on SimpleUrlHandlerMapping");
-		} else {
-			for (Map.Entry<String, Object> entry : urlMap.entrySet()) {
-				String url = entry.getKey();
-				Object handler = entry.getValue();
-				// Prepend with slash if not already present.
-				if (!url.startsWith("/")) {
-					url = "/" + url;
-				}
-				if (!url.endsWith("/")) {
-					url += "/";
-				}
-				// Remove whitespace from handler bean name.
-				if (handler instanceof String) {
-					handler = ((String) handler).trim();
-				}
-				url += "**";// match all url in this path ..
-				registerHandler(url, handler);
-			}
-		}
-	}
+    @Override
+    protected void registerHandlers(Map<String, Object> urlMap) throws BeansException {
+        if (urlMap.isEmpty()) {
+            logger.warn("Neither 'urlMap' nor 'mappings' set on SimpleUrlHandlerMapping");
+        } else {
+            for (Map.Entry<String, Object> entry : urlMap.entrySet()) {
+                String url = entry.getKey();
+                Object handler = entry.getValue();
+                // Prepend with slash if not already present.
+                if (!url.startsWith("/")) {
+                    url = "/" + url;
+                }
+                if (!url.endsWith("/")) {
+                    url += "/";
+                }
+                // Remove whitespace from handler bean name.
+                if (handler instanceof String) {
+                    handler = ((String) handler).trim();
+                }
+                // match all url in this path ..
+                url += "**";
+                registerHandler(url, handler);
+            }
+        }
+    }
 }
